1e68d631634a71596fb5636b68b586318ba06439
ccantoral2121 <cristianggerardo@hotmail.com>
2019-03-08 09:52:02 -0500

    Test Map

--- src/main/java/edu/ithaca/gamemaster/map/Map.java
+++ src/main/java/edu/ithaca/gamemaster/map/Map.java
@@ -2,7 +2,22 @@ package edu.ithaca.gamemaster.map;
 
 public class Map {
 
+    private int width;
+    private int heigth;
+    private String image;
+
+    public Map(int width, int length, String image) {
+
+        this.width = width;
+        this.heigth = length;
+        this.image = image;
+
+    }
 
 
+    public int getWidth(){
+
+        return width;
+    }
 
 }
---
+++ src/test/java/edu/ithaca/gamemaster/MapTest.java
@@ -0,0 +1,19 @@
+package edu.ithaca.gamemaster;
+
+import edu.ithaca.gamemaster.map.Map;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MapTest {
+
+
+    @Test
+    void CreateMap(){
+
+        //Can't create a map without string name
+        assertThrows(IllegalArgumentException.class, () ->new Map(10,10,"") );
+
+    }
+
+
+}
