be546cea67c3264f95ab5265ae7ebb1fbcaa2d80
mramir3z <mramirez@ithaca.edu>
2019-03-22 00:48:58 -0400

    Autogen of stats completed and working with API Manual generation pending.

--- src/main/java/edu/ithaca/gamemaster/CharacterUI.java
+++ src/main/java/edu/ithaca/gamemaster/CharacterUI.java
@@ -1,11 +1,16 @@
 package edu.ithaca.gamemaster;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class CharacterUI {
     Scanner input = new Scanner(System.in);
     DiceRoll rand = new DiceRoll(4,6);
+    Random r = new Random();
+    String sampleLanguages[] = new String[]{"Common", "Dwarvish ", "Elvish", "Gnomish", "Goblin", "Halfing", "Orc"};
+    String alignments[] = new String[]{"Lawful good", "Lawful neutral ", "Lawful evil", "Neutral good", "True neutral", "Neutral evil", "Chaotic good", "Chaotic neutral", "Chaotic evil"};
 
     //create character
     public void createNewCharacter() {
@@ -32,7 +37,11 @@ public class CharacterUI {
 
             if (isValidYesNo(YN)) { //double checking
                 System.out.print("Manual confirmed. Begin: ");
-                //TODO: call all the setters
+                //TODO: call all the setters manually
+                    //get stat input value (or dice roll "DiceRoll")
+                    //check for valid input
+                    //call setter
+                System.out.println("Actually, you can't do this yet. Try auto-gen instead!");
             }
         }
         if (!isValidYesNo(YN)){
@@ -42,27 +51,57 @@ public class CharacterUI {
             if (isValidYesNo(YN)) { //double checking
                 System.out.println("Auto-gen confirmed. Starting...");
                 newC.randomizeAll();
-                newCharacterHitAndHPStuff(newC);
-                printStats(newC);
+                randCharacterHitAndHPStuff(newC);
             }
         }
     }
 
     //this seems iffy. pull?
-    public void newCharacterHitAndHPStuff(Character newC){
+    public void randCharacterHitAndHPStuff(Character newC){
         newC.setHitDice(rand.abilityRoll(), rand.abilityRoll(), rand.abilityRoll());
         newC.setHP(rand.abilityRoll());
         newC.setStrength(rand.abilityRoll());
         newC.setArmor(rand.abilityRoll());
         newC.setSpeed(rand.abilityRoll());
+
+        randCharacterActsLangsAlign(newC);
     }
 
-    //if manual, time for hell aka call all setters individually
-    //get stat input value (or dice roll "DiceRoll")
-    //check for valid input
-    //call setter
+    public void randCharacterActsLangsAlign(Character newC){
+        ArrayList<String> languages = new ArrayList<>();
+        ArrayList<Action> actions = new ArrayList<>();
 
-    public void printStats (Character newC)  { //will throw a nullpointerexception if called before all stats are set.
+        //Alignment
+        newC.setAlignment(alignments[r.nextInt(8)]); // r.nextInt returns int from 0 to the given parameter
+
+        //Langs
+        System.out.print("How many languages? (no more than 7) \n > ");
+        int quant = input.nextInt();
+        while (quant < 0 || quant > 7) { //nothing under 0 nor over 7
+            System.out.print("Invalid. \n How many languages? \n > ");
+            quant = input.nextInt();
+        }
+        for (int i = 0; i < quant ; i++) {
+            //Review! This line allows repeat numbers, therefore repeat languages.
+            languages.add(sampleLanguages[(int) r.nextInt(6)]); // minus one because arrays
+        }
+
+        //Actions - NOT actually random yet. Everyone gets Fireball. Review?
+        Action fireball = new Action("Fireball",5,1,8,"Fire");
+        actions.add(fireball);
+
+        //Setting lists to character
+        newC.setLanguages(languages);
+        newC.setActions(actions); //Review; maybe Actions can have its own toString definition?
+
+        //commented because of tests.
+        //printStats(newC);
+    }
+
+    //pull?
+    //prints stats. you can call this anywhere, really, as long as you've made the character fully and completely.
+    //Actions print out like "[edu.ithaca.gamemaster.Action@1753acfe]". review?
+    public static void printStats(Character newC) { //will throw a nullpointerexception if called before all stats are set.
         Field newCdata[] = newC.getClass().getDeclaredFields();
         Field forDisplay; //its a temp var
 
@@ -72,7 +111,7 @@ public class CharacterUI {
 
             try {
                 System.out.println(forDisplay.getName() + ": " + forDisplay.get(newC).toString());
-            } catch (IllegalAccessException | NullPointerException ne){ System.out.println( ne.toString() + " caught in CharacterUI.printStats() =("); }
+            } catch (IllegalAccessException | NullPointerException ne){ System.out.println( ne.toString() + " caught in CharacterUI.printStats() "); }
         }
     }
 
@@ -97,4 +136,6 @@ public class CharacterUI {
         CUI.createNewCharacter();
         System.out.println("main done.");
     }
+
+
 }
