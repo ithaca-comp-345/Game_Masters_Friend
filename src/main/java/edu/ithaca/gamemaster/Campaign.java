2c08de830524d7ab816cfa5a8682999a52e81c0b
Tea Mdevadze <tmdevadze@ithaca.edu>
2019-04-04 00:55:34 -0400

    campaign addNotes + getnotes

--- src/main/java/edu/ithaca/gamemaster/Campaign.java
+++ src/main/java/edu/ithaca/gamemaster/Campaign.java
@@ -1,16 +1,19 @@
 package edu.ithaca.gamemaster;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Campaign{
     public String campaignName;
-    //public Location location;
+    public Location location;
     public Map<String,PlayerUser> userPlayers;
+    public Map<String,Notes> notes;
 
 
     public Campaign(){
-        userPlayers=new HashMap<>();
+       this.notes = new HashMap<>();
+       this.userPlayers=new HashMap<>();
     }
 
     public void addUserPlayers(String username, PlayerUser playerUser){
@@ -32,10 +35,36 @@ public class Campaign{
         }
     }
 
-   // public void shareInformation(){};
-   // public void createEncounter(){};
-   // public void addNotes(){};
+    //  `public Location searchLocations(String name){ return location}
 
+
+
+    public void addNotes(String noteName, String text){
+        Notes note=new Notes(noteName,text);
+        notes.put(noteName,note);
+    }
+
+    public String getNotes(String noteName){
+        if(notes.containsKey(noteName)){
+            return notes.get(noteName).getNotes();
+        }
+        throw new IllegalArgumentException("notes were not found");
+    }
+
+
+
+   // public void editNotes(){};
+
+
+   // public void shareInformation(){};
+   // public void createEncounter(){}
+    // public void addNotes(String campaignName){};
+    // public void editCharacter(){};
+    // public void editLocation(){};
+    // public void editNPC();
+    // public void editMap();
+    // public void searchNPC();
+    // public void shareMap();
 
 
 }
