f3df26145eb6d976c93550cfa45720c5ebeab1e2
Tea Mdevadze <tmdevadze@ithaca.edu>
2019-04-08 20:52:47 -0400

    Notes Test updated

--- src/test/java/edu/ithaca/gamemaster/notesTest.java
+++ src/test/java/edu/ithaca/gamemaster/notesTest.java
@@ -6,15 +6,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class notesTest {
 
-    Notes testNote = new Notes("Foo", "Bar");
 
+    @Test
     void createNoteTest(){
+        Notes testNote1 = new Notes("Foo", "Bar");
+        Notes testNote2 = new Notes("one", "");
+        Notes testNote3 = new Notes("two", " ");
+        Notes testNote4 = new Notes("three", "\n");
 
-        assertEquals("Foo",testNote.getNoteName());
-        assertEquals("Bar",testNote.getNotes());
+        assertEquals("Foo",testNote1.getNoteName());
+        assertEquals("Bar",testNote1.getNotes());
+
+        assertEquals("one",testNote2.getNoteName());
+        assertEquals("",testNote2.getNotes());
+        assertEquals("two",testNote3.getNoteName());
+        assertEquals(" ",testNote3.getNotes());
+        assertEquals("three",testNote4.getNoteName());
+        assertEquals("\n",testNote4.getNotes());
+
+        assertThrows(IllegalArgumentException.class, ()-> new Notes("Foo", "lol"));
     }
-    void addToNotesTest(){
-        testNote.addToNotes("raB");
+
+    @Test
+    void editNotesTest(){
+        Notes testNote1 = new Notes("Foo", "Bar");
+
+        testNote1.editNotes("");
+        assertEquals("", testNote1); //everything is deleted
+        testNote1.editNotes("Foo"); //"Foo" is entered
+        assertEquals("Foo",testNote1);
+
+        testNote1.editNotes("Foo Foo"); //"Foo" is entered
+        assertEquals("Foo Foo",testNote1);
+
 
     }
 }
