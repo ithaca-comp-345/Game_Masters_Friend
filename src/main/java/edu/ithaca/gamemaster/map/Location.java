72c67fd6926406fa74b7e08ea85fe67d89d8f015
Tea Mdevadze <tmdevadze@ithaca.edu>
2019-04-11 09:55:42 -0400

    location

--- src/main/java/edu/ithaca/gamemaster/map/Location.java
+++ src/main/java/edu/ithaca/gamemaster/map/Location.java
@@ -1,7 +1,24 @@
 package edu.ithaca.gamemaster.map;
 
 public class Location {
-    public String name;
+    public String locationName;
+    public Map map;
+    public String imagePath;
 
+    public Location(String imagePathIn, String locationNameIn){
+        this.locationName = locationNameIn;
+        this.imagePath =imagePathIn;
+        this.map = new Map();
+    }
+
+    public void editMap(String imagePathIn){
+        //this.map;
+    };
+
+    public void getMap(){
+
+    };
+
+    public void copyMap(){};
 
 }
