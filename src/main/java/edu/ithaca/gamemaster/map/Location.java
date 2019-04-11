04e5b66144a668676c46329f1b7c8c3db1d3fb55
Tea Mdevadze <tmdevadze@ithaca.edu>
2019-04-11 11:25:11 -0400

    Location locationTest exception

--- src/main/java/edu/ithaca/gamemaster/map/Location.java
+++ src/main/java/edu/ithaca/gamemaster/map/Location.java
@@ -1,15 +1,32 @@
 package edu.ithaca.gamemaster.map;
 
+import java.io.File;
+
 public class Location {
     public String locationName;
     public Map map;
     public String imagePath;
 
     public Location(String imagePathIn, String locationNameIn){
-        this.locationName = locationNameIn;
-        this.imagePath = imagePathIn;
-        this.map = new Map(imagePath);
+        if(isPathValid(imagePathIn)){
+            this.locationName = locationNameIn;
+            this.imagePath = imagePathIn;
+            this.map = new Map(imagePath);
+        }
+        else {
+            throw new IllegalArgumentException("path is incorrect");
+        }
     }
+
+   public static boolean isPathValid(String imagePathIn){
+       boolean exists = (new File(imagePathIn)).exists();
+       if (exists){
+           return true;
+       }
+       else {
+           return false;
+       }
+   }
 
     public void editMap(String imagePathIn){
         this.map =  new Map(imagePath);
