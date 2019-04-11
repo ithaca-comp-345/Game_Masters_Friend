88e69e1ebeb683e2aabb6066ac8acfdb5f3ea5a5
divinefault <divinefaultcs@gmail.com>
2019-04-11 17:50:07 -0400

    added statCheck and statCheckTest

---
+++ src/test/java/edu/ithaca/gamemaster/statCheckTest.java
@@ -0,0 +1,15 @@
+package edu.ithaca.gamemaster;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class statCheckTest {
+
+    Character testChar = new Character("Fauxcus Moore");
+    @Test
+    void skillCheckTest(){
+
+
+    }
+}
